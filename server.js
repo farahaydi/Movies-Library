61ec7bb672ae97fc63e2ed54a1711b56896e2ca7
farahisJoy <farahelaydi9@gmail.com>
2023-07-05 13:34:13 +0000

    Labe

--- server.js
+++ server.js
@@ -1,28 +1,26 @@
-'use strict'
-const express = require("express");
-const cors = require("cors");
+'use strict';
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
-const axios = require("axios");
-
+const axios = require('axios');
 
 const movieRoute = require('./Route/movies.routes');
 const mainRoute = require('./Route/mainRoute.routes');
-const notFound =require('./errorHandler/404');
-const internalError =require('./errorHandler/500');
+const notFound = require('./errorHandler/404');
+const internalError = require('./errorHandler/500');
 
-const dbconection = require("./connection")
- 
+const dbconection = require('./connection');
+
 let server = express();
 server.use(express.json());
 
-dbconection.connect().then(()=>{
-  server.listen(3000, ()=>{
-    console.log('Server is running')
-    });
-
+dbconection.connect().then(() => {
+  server.listen(3000, () => {
+    console.log('Server is running');
+  });
 });
 
 server.use(mainRoute);
 server.use(movieRoute);
-server.use(notFound); 
+server.use(notFound);
 server.use(internalError);
