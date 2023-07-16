72d4193768ccf7731fb89a368cde586a8960e313
farahisJoy <farahelaydi9@gmail.com>
2023-07-16 08:27:43 +0000

    newest

--- Route/movies.routes.js
+++ Route/movies.routes.js
@@ -7,8 +7,19 @@
 "use strict";
 const express = require("express");
 const dbconection = require("../connection");
+const axios =require('axios');
 const Router = express.Router();
 
+
+
+function Movie(title,poster_path,overview,release_date,id)
+{
+this.title = title;
+this.poster_path=poster_path;
+this.overview = overview;
+this.release_date=release_date;
+this.id=id;
+}
 
 
 Router.post('/addMovie', (req,res, next)=>
